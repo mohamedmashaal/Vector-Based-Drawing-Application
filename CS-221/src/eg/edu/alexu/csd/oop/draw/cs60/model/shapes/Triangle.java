b2ab3ea0943f31e2d3e612361c6348cb5ef2cdbd
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-10-23 15:03:15 +0200

    fixing some stuff

--- CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/shapes/Triangle.java
+++ CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/shapes/Triangle.java
@@ -1,5 +1,13 @@
 package eg.edu.alexu.csd.oop.draw.cs60.model.shapes;
 
+import java.awt.Graphics;
+
 public class Triangle extends MainShape{
+
+	@Override
+	public void draw(Graphics canvas) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
