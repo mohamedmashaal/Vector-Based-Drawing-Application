4e6f81cc85b3a490b6a9c9dd0c49e6bd946c5006
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-10-23 12:23:42 +0200

    Aha

---
+++ CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/DrawEngineImp.java
@@ -0,0 +1,71 @@
+package eg.edu.alexu.csd.oop.draw.cs60;
+
+import java.awt.Graphics;
+import java.util.List;
+
+import eg.edu.alexu.csd.oop.draw.DrawEngine;
+import eg.edu.alexu.csd.oop.draw.Shape;
+
+public class DrawEngineImp implements DrawEngine {
+
+	@Override
+	public void refresh(Graphics canvas) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addShape(Shape shape) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeShape(Shape shape) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateShape(Shape oldShape, Shape newShape) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Shape[] getShapes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Class<? extends Shape>> getSupportedShapes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void redo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save(String path) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void load(String path) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
