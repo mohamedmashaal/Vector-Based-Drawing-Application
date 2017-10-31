18e722c517604627d069b464dfeab1bbd5eee552
Youssef_Ali <youssefali6212@gmail.com>
2017-10-31 21:10:35 +0200

    if condition for load method

--- CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/DrawEngineImp.java
+++ CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/DrawEngineImp.java
@@ -154,6 +154,10 @@ public class DrawEngineImp implements DrawingEngine {
 	@Override
 	public void load(String path) {
 		//throw new RuntimeException(path);
+		if(!path.substring(path.length()-3).equalsIgnoreCase("xml")
+				&& !path.substring(path.length()-4).equalsIgnoreCase("json")){
+					throw new RuntimeException(path);
+				}
 		loadXML(path);
 		loadJSON(path);
 	}
