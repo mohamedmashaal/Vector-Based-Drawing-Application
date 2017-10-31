f51504b6ccfaaa0385dedfbcd43f2d78ee1f3f9e
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-10-31 22:01:19 +0200

    Tester shit again

--- CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/DrawEngineImp.java
+++ CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/DrawEngineImp.java
@@ -118,10 +118,7 @@ public class DrawEngineImp implements DrawingEngine {
 	@Override
 	public Shape[] getShapes() {
 		// TODO Auto-generated method stub
-		if(shapes.peek().isEmpty()) {
-			return null ;
-		}
-		return shapes.peek().toArray(new Shape[0]);
+		return shapes.peek().toArray(new Shape[1]);
 	}
 	
 	@Override
