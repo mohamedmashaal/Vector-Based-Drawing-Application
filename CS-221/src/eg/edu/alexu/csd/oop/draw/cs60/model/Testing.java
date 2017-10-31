aaddff495a9b5305c1a600faf54d15102bae7874
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-10-31 21:23:43 +0200

    tester Shit

--- CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/Testing.java
+++ CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/Testing.java
@@ -11,21 +11,21 @@ public class Testing {
 	public static void main(String [] args) {
 		DrawEngineImp engine = DrawEngineImp.getUniqueInstance();
 		//List<Class<? extends Shape>> list = engine.getSupportedShapes();
-		Shape Z = new Circle();
-		/*List<Class<? extends Shape>> list = engine.getSupportedShapes();
+		/*Shape Z = new Circle();
+		List<Class<? extends Shape>> list = engine.getSupportedShapes();
 		for(Class x :list) {
 			System.out.println(x.getName());
-		}*/
+		}
 		Circle circle = new Circle();
 		circle.setPosition(new Point(2,2));
 		engine.addShape(circle);
 		engine.save("file");
-		engine.clear();
+		//engine.clear();
 		engine.load("file");
 		Shape [] shapes = engine.getShapes();
 		for(Shape x : shapes) {
 			System.out.println(x);
 			System.out.println(x.getPosition());
-		}
+		}*/
 	}
 }
