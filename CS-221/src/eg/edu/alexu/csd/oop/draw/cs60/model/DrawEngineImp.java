a2c0611231380e5694cf8fe3e2a296834a2745e1
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-11-09 21:48:46 +0200

    Alright .... !!

--- CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/DrawEngineImp.java
+++ CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/DrawEngineImp.java
@@ -282,7 +282,7 @@ public class DrawEngineImp implements DrawingEngine , Subject {
 			//notifyObservers();
 	        }
 		catch(Exception e) {
-			throw new RuntimeException(shapesXMLContent.toString());
+			throw new RuntimeException("\""+ shapesXMLContent.toString()+"\"");
 		}
 	}
 	
