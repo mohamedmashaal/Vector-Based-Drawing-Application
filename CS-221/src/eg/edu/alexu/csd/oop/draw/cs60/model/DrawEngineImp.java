ccab38159a558b5c59b9d957f6149df775724f19
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-11-09 21:20:52 +0200

    load and undo

--- CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/DrawEngineImp.java
+++ CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/DrawEngineImp.java
@@ -276,7 +276,9 @@ public class DrawEngineImp implements DrawingEngine , Subject {
 			}
 			in.close();
 	        Stack<ArrayList<Shape>> parsedObj = (Stack<ArrayList<Shape>>) stringToObject(shapesXMLContent.toString());
-	        shapes = parsedObj;
+	        shapes = new Stack<>();
+	        shapes.push(parsedObj.peek());
+	        //shapes = parsedObj;
 	        notifyObservers();
 	        }
 		catch(Exception e) {
