7d93838db2d91037b8204306539c38e9496fa004
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-11-11 17:17:07 +0200

    TS

--- CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/XMLParser.java
+++ CS-221/src/eg/edu/alexu/csd/oop/draw/cs60/model/XMLParser.java
@@ -8,9 +8,14 @@ import org.xml.sax.*;
 import org.w3c.dom.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -55,7 +60,7 @@ public class XMLParser {
                 Transformer tr = TransformerFactory.newInstance().newTransformer();
                 tr.setOutputProperty(OutputKeys.INDENT, "yes");
                 tr.setOutputProperty(OutputKeys.METHOD, "xml");
-                //tr.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
+                tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
                 //tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "shapes.dtd");
                 tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
@@ -77,6 +82,20 @@ public class XMLParser {
     public ArrayList<Map<String, String>> readXML(File xml) {
         ArrayList<Map<String, String>> shapesMaps = new ArrayList<>();
         Document dom;
+        InputStream inputStream;
+        Reader reader = null;
+		try {
+			inputStream = new FileInputStream(xml);
+			reader = new InputStreamReader(inputStream,"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        InputSource is = new InputSource(reader);
+        is.setEncoding("UTF-8");
         // Make an  instance of the DocumentBuilderFactory
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         try {
