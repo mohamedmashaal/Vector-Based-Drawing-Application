58a4c8fccf5192ff09e6d7e914f63ad007d26a9d
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-11-21 23:13:23 +0200

    Testing edit

--- CS-221/src/eg/edu/alexu/csd/oop/db/cs60/View/Testing.java
+++ CS-221/src/eg/edu/alexu/csd/oop/db/cs60/View/Testing.java
@@ -8,23 +8,12 @@ import eg.edu.alexu.csd.oop.db.cs60.Model.DatabaseImp;
 public class Testing {
 
     public static void main(String[] args){
-        //SELECT * From table_name12 SELECT * From table_name12
-    	//SELECT column_name1 FROM table_name13 WHERE coluMN_NAME2 < 5 SELECT column_name1 FROM table_name13 WHERE coluMN_NAME2 < 5
-    	//SELECT * FROM table_name1 WHERE coluMN_NAME2 < 6 SELECT * FROM table_name1 WHERE coluMN_NAME2 < 6
-    	// UPDATE table_name8 SET column_name1='11111111', COLUMN_NAME2=22222222, column_name3='333333333' WHERE coLUmn_NAME3='VALUE3' UPDATE table_name8 SET column_name1='11111111', COLUMN_NAME2=22222222, column_name3='333333333' WHERE coLUmn_NAME3='VALUE3'
-    	//DELETE From table_name2
-    	//DELETE From table_name2 WHERE coLUmn_NAME1='VAluE1'
-    	//UPDATE table_namE1 SET column_name1 11111111 COLUMN_NAME2 22222222 column_name3 333333333 WHERE coLUmn_NAME3 VALUE3
-    	String x = "SELECT column_name1 FROM table_name13 WHERE coluMN_NAME2 < 5";
-        //String x = "	sons (PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255) )";
-        //Create TABLE table_name1(column_name1 varchar, column_name2 int, column_name3 varchar)
-        // INSERT INTO table_name8(column_NAME1, COLUMN_name3, column_name2) VALUES ('value1', 'value3', 4) INSERT INTO table_name8(column_NAME1, COLUMN_name3, column_name2) VALUES ('value1', 'value3', 4)
-        //UPDATE table_name9 SET column_name1='value1', column_name2=15, column_name3='value2' UPDATE table_name9 SET column_name1='value1', column_name2=15, column_name3='value2'
-        x = x.replaceAll("\\)", " ").replaceAll("\\(", " ").replaceAll("'", "").replaceAll("\\s+\\,", ",");
+        
+        /*x = x.replaceAll("\\)", " ").replaceAll("\\(", " ").replaceAll("'", "").replaceAll("\\s+\\,", ",");
         System.out.println(x);
         String [] z = x.split("\\s+|\\,\\s*|\\(|\\)|\\=");
         for(String v : z)
-        	System.out.println(v);
+        	System.out.println(v);*/
 
     	/*DatabaseImp db = new DatabaseImp();
     	db.createDatabase("7amada", true);
